444a656e422bbb154c11b82bb2336d2a18c8e299
eellpp <6888621+eellpp@users.noreply.github.com>
2023-08-06 12:45:07 +0800

    Create Post RequestExample.svelte

---
+++ Post RequestExample.svelte
@@ -0,0 +1,37 @@
+<script>
+	let title = 'This is a example post';
+	let user = 'johndoe';
+	let userId = 2;
+	let result = '';
+	
+	async function doPost () {
+		let postData = JSON.stringify({
+				title,
+				userId,
+				user
+			});
+		console.log(postData);
+		const res = await fetch('https://dummyjson.com/posts/add', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			  body: postData
+		});
+		
+		const json = await res.json()
+		result = JSON.stringify(json)
+	}
+</script>
+
+
+<input bind:value={title} />
+<input bind:value={user} />
+<input bind:value={userId} />
+<button type="button" on:click|preventDefault={doPost}>
+	Post it.
+</button>
+<p>
+	Result:
+</p>
+<pre>
+{result}
+</pre>
